a031b3a2d8f72b0a2c5e9a5304d2f5061760a46d
Ching <marcrohannmching@iskolarngbayan.pup.edu.ph>
2024-01-23 02:24:24 +0800

    finished code

--- exercise 10.py
+++ exercise 10.py
@@ -14,11 +14,8 @@ def combine_list(first_list, second_list):
     for i in range(0, 5, 1):
         if second_list[i] % 2 == 0:
             final_list.append(second_list[i])
-
     
     print("the result is", final_list)
-
-
 
 first_list = [10, 20, 25, 30, 35]
 second_list = [40, 45, 60, 75, 90]
